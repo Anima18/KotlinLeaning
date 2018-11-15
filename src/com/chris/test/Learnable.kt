63717f1596d2a9d41a5a32070265cad16fd3634e
Anima18 <591151451@qq.com>
2018-11-15 11:06:31 +0800

    interface test

---
+++ src/com/chris/test/Learnable.kt
@@ -0,0 +1,9 @@
+package com.chris.test
+
+interface Learnable {
+    var flag: Boolean
+    fun reading()
+    fun write() {
+        println("person are write")
+    }
+}
--- src/com/chris/test/Student.kt
+++ src/com/chris/test/Student.kt
@@ -1,19 +1,31 @@
 package com.chris.test
 
-class Student(var name:String) {
+class Student(name:String) : Learnable {
     var age = 0
+    var name: String? = null
+    override var flag: Boolean = false
     constructor(name:String, age:Int) : this(name) {
         this.age = age
         println("age : ${this.age}")
     }
     init {
         println("student $name init")
-
+        this.name = name
+        flag = true
     }
 
+    override fun reading() {
+        println("$name is reading")
+    }
 
+    override fun write() {
+        super.write()
+        println("$name is write")
+    }
 }
 
 fun main(args : Array<String>) {
     var student = Student("Chris", 25)
+    student.reading()
+    student.write()
 }
